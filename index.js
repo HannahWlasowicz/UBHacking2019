5d9fdbc222d1d312b58f796d8702cc152d43a82c
Hannah Wlasowicz <hannahwl@buffalo.edu>
2019-11-02 17:37:06 -0400

    Removed the additional message

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 //test
-const token = 'NjQwMjgzNjEwNDU2NTIyNzUy.Xb3qvQ.aVNwLMqlN_Pb3FHFjy__z_S1AQw
-';
-
-bot.on('ready', ()=>{
-  console.log("This bot is online!");  
+const token = 'NjQwMjgzNjEwNDU2NTIyNzUy.Xb3qvQ.aVNwLMqlN_Pb3FHFjy__z_S1AQw';
+bot.on('ready',()=>{
+  console.log('This bot is online!')
 })
 
 bot.on('message', msg=>{
