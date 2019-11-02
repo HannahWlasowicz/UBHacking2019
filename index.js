2591f0997cc126097d9055d5887cf53c6959a71a
Gillian Marcus <Gillian@Gillians-MacBook-Pro.local>
2019-11-02 17:33:16 -0400

    lol

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ const bot = new Discord.Client();
 const token = 'NjQwMjgzNjEwNDU2NTIyNzUy.Xb3qvQ.aVNwLMqlN_Pb3FHFjy__z_S1AQw
 ';
 
+bot.on('ready', ()=>{
+  console.log("This bot is online!");  
+})
+
+bot.on('message', msg=>{
+    if (msg.content==="slut"){
+        msg.reply('you know it lol');
+    }
+})
+
 bot.login(token);
