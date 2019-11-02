ae1bb76490a2a4e9a4ad6a4da1b064217c48a05b
Hannah Wlasowicz <hannahwl@buffalo.edu>
2019-11-02 17:09:41 -0400

    Allows the bot to log online

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 //test
+const token = 'NjQwMjgzNjEwNDU2NTIyNzUy.Xb3qvQ.aVNwLMqlN_Pb3FHFjy__z_S1AQw
+';
+
+bot.login(token);
