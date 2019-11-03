4a55727e46e2ef2ce3e07021185557c7da3f7670
Hannah Wlasowicz <hannahwl@buffalo.edu>
2019-11-02 23:08:57 -0500

    npm download

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import sqlite3
 
 const sqlite3 = require('sqlite3');
+var link = '';
 const db = new sqlite3.Database('discord.db', sqlite3.OPEN_READONLY, (err) =>{
     if(err){
         console.error(err.message);
@@ -8,11 +8,15 @@ const db = new sqlite3.Database('discord.db', sqlite3.OPEN_READONLY, (err) =>{
     console.log('Connected to the discord database.');
 });
 
-db.each("SELECT url FROM invite_links WHERE id IN (SELECT id FROM invite_links ORDER BY RANDOM() LIMIT 1)", (err, row) => {
+db.each("SELECT url FROM invite_links WHERE id IN (SELECT id FROM invite_links ORDER BY RANDOM() LIMIT 1)", join(err, row) => {
     if (err){
         console.error(err.message);
     }
-  console.log("URL:" row.url);
+  console.log("URL:"+row.url);
+  link = row.url;
+
+  console.log(link);
+  console.log("Printed link");
 });
 
 
