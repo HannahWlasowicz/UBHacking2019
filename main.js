4f285385f486df233b9f9fdbb5804b58f1fed80a
Hannah Wlasowicz <hannahwl@buffalo.edu>
2019-11-02 21:36:28 -0500

    Created database query

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ import sqlite3
 
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('discord.db');
+
+db.each("SELECT url FROM invite_links ORDER BY RANDOM() LIMIT 1)", function(err, row)) {
+  console.log("URL: "row.url);
+});
+db.close();
