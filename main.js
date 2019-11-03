707f7e7b2454f8301811c87ba0e6a6cbd7caa56e
Gillian Marcus <Gillian@Gillians-MacBook-Pro.local>
2019-11-02 22:59:28 -0400

    db stuff

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 import sqlite3
 
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('discord.db');
-
-db.each("SELECT url FROM invite_links ORDER BY RANDOM() LIMIT 1)", function(err, row)) {
-  console.log("URL: "row.url);
+const db = new sqlite3.Database('discord.db', sqlite3.OPEN_READONLY, (err) =>{
+    if(err){
+        console.error(err.message);
+    }
+    console.log('Connected to the discord database.');
 });
-db.close();
+
+db.each("SELECT url FROM invite_links ORDER BY RANDOM() LIMIT 1)", (err, row) => {
+    if (err){
+        console.error(err.message);
+    }
+  console.log("URL:" row.url);
+});
+
+
+db.close((err) => {
+    if(err){
+        console.error(err.message);
+    }
+    console.log('Close the database connection.');
+});
